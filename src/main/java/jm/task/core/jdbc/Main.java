af0c0d2a640d90eb8ed3d50ecce92a73e41d0274
vorona80 <vorona_80@mail.ru>
2023-05-01 13:27:15 +0300

    change2

--- src/main/java/jm/task/core/jdbc/Main.java
+++ src/main/java/jm/task/core/jdbc/Main.java
@@ -1,6 +1,7 @@
 package jm.task.core.jdbc;
 
 import jm.task.core.jdbc.dao.UserDaoJDBCImpl;
+import jm.task.core.jdbc.model.User;
 import jm.task.core.jdbc.service.UserService;
 import jm.task.core.jdbc.service.UserServiceImpl;
 import jm.task.core.jdbc.util.Util;
@@ -10,19 +11,22 @@ import java.sql.SQLException;
 public class Main {
     public static void main(String[] args) throws SQLException {
      //   Util.getconnection(); //проверка подключения к базе данны
+        User user = new User();
         UserServiceImpl userService = new UserServiceImpl();
         userService.createUsersTable();
         userService.saveUser("Name1", "LastName1", (byte) 20);
-        System.out.println(userService.getAllUsers().toString());
+        user = userService.getAllUsers().get(0);
+        System.out.println("User с именем - " + user.getName() + " добавлен в базу данных.");
         userService.saveUser("Name2", "LastName2", (byte) 25);
-        System.out.println(userService.getAllUsers().toString());
+        System.out.println("User с именем - " + userService.getAllUsers().get(1).getName() + " добавлен в базу данных.");
         userService.saveUser("Name3", "LastName3", (byte) 31);
-        System.out.println(userService.getAllUsers().toString());
+        System.out.println("User с именем - " + userService.getAllUsers().get(2).getName() + " добавлен в базу данных.");
         userService.saveUser("Name4", "LastName4", (byte) 38);
-        System.out.println(userService.getAllUsers().toString());
+        System.out.println("User с именем - " + userService.getAllUsers().get(3).getName() + " добавлен в базу данных.");
+        System.out.println(userService.getAllUsers());
         userService.cleanUsersTable();
         userService.dropUsersTable();
-    //    userDaoJDBC.removeUserById(3);
+        //    userDaoJDBC.removeUserById(3);
         Util.slose();
 
     }
